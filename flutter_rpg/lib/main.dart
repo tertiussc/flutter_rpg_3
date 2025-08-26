56bc24f74832ef4496eac6b25fbed2a49dc4f371
Tertius Scheepers <tertiussc@gmail.com>
2025-08-26 16:27:18 +0200

    Added home screen basic and sandbox

--- flutter_rpg/lib/main.dart
+++ flutter_rpg/lib/main.dart
@@ -2,5 +2,27 @@ import 'package:flutter/material.dart';
 import 'screens/home/home.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home()));
+  runApp(
+    MaterialApp(
+      home: Home(),
+    ),
+  );
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Placeholder(),
+      ),
+    );
+  }
 }
