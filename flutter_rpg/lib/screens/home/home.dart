84225650263cc84d2914e0c92667f0e900b76ed3
Tertius Scheepers <tertiussc@gmail.com>
2025-09-04 07:12:13 +0200

    Added a styled button

--- flutter_rpg/lib/screens/home/home.dart
+++ flutter_rpg/lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 
@@ -20,15 +21,13 @@ class _HomeState extends State<Home> {
       body: Container(
         padding: EdgeInsets.all(16),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             StyledText('Character List 1'),
             StyledHeading('Character list 2'),
             StyledTitle('Character list 3'),
-            FilledButton(
-              style: FilledButton.styleFrom(backgroundColor: AppColors.primaryColor),
+            StyledButton(
               onPressed: () {},
-              child: Text('Create new'),
+              child: StyledHeading('Create new'),
             )
           ],
         ),
