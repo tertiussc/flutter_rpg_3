b3dc0a39903d25024816c6bc6abaf4f1e6fb924e
Tertius Scheepers <tertiussc@gmail.com>
2025-10-03 08:18:10 +0200

    Added firestore converters

--- flutter_rpg/lib/services/firestore_services.dart
+++ flutter_rpg/lib/services/firestore_services.dart
@@ -1,5 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_rpg/models/character.dart';
 
 class FireStoreService {
-  static final ref = FirebaseFirestore.instance.collection('characters');
+  static final ref = FirebaseFirestore.instance.collection('characters').withConverter(
+        fromFirestore: Character.fromFirestore,
+        toFirestore: (Character c, _) => c.toFirestore(),
+      );
 }
