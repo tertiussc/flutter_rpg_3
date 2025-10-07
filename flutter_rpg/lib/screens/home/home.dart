3b21a66664ad48f7d6c72ba0f2af81d35d5ede93
Tertius Scheepers <tertiussc@gmail.com>
2025-10-07 07:47:57 +0200

    added dismissable (swipe) and hooked up the dlete function to delete characters

--- flutter_rpg/lib/screens/home/home.dart
+++ flutter_rpg/lib/screens/home/home.dart
@@ -37,7 +37,16 @@ class _HomeState extends State<Home> {
                 return ListView.builder(
                     itemCount: value.characters.length,
                     itemBuilder: (_, index) {
-                      return CharacterCard(value.characters[index]);
+                      return Dismissible(
+                        key: ValueKey(value.characters[index].id),
+                        direction: DismissDirection.endToStart,
+                        onDismissed: (direction) {
+                          Provider.of<CharacterStore>(context, listen: false).removeCharacter(value.characters[index]);
+                        },
+                        child: CharacterCard(
+                          value.characters[index],
+                        ),
+                      );
                     });
               }),
             ),
