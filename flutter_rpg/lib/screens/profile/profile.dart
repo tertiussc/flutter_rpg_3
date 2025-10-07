fe368684673c81cb2f8bff92531b23c114b0586f
Tertius Scheepers <tertiussc@gmail.com>
2025-10-07 07:48:22 +0200

    Added Hero animation

--- flutter_rpg/lib/screens/profile/profile.dart
+++ flutter_rpg/lib/screens/profile/profile.dart
@@ -32,10 +32,13 @@ class Profile extends StatelessWidget {
               color: AppColors.secondaryColor.withValues(alpha: 0.3),
               child: Row(
                 children: [
-                  Image.asset(
-                    'assets/img/vocations/${character.vocation.image}',
-                    width: 140,
-                    height: 140,
+                  Hero(
+                    tag: character.id.toString(),
+                    child: Image.asset(
+                      'assets/img/vocations/${character.vocation.image}',
+                      width: 140,
+                      height: 140,
+                    ),
                   ),
                   SizedBox(width: 20),
                   Expanded(
